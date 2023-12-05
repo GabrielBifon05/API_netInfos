1a7f178c7a9f968924e1ca5ffc777ec5b4893512
Gabriel Bifon de Souza <125585322+GabrielBifon05@users.noreply.github.com>
2023-12-05 12:19:21 -0300

    Update capturasNet.py

--- capturasNet.py
+++ capturasNet.py
@@ -165,8 +165,6 @@ while True:
 
     def getHwAddr(ifname):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
-        return ':'.join('%02x' % b for b in info[18:24])
 
         netStats1 = psutil.net_io_counters()
         dataSent = netStats1.bytes_sent
